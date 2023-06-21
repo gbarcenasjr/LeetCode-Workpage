f6224ff7c36fb252434b78656a39c5d42d057146
gbarcenasjr <gbarcenasjr@gmail.com>
2023-06-21 15:50:49 -0700

    Optimized and Explained Solution to Question 36

--- Python-Solutions/q-0036.py
+++ Python-Solutions/q-0036.py
@@ -43,61 +43,51 @@ Since there are two 8's in the top left 3x3 sub-box, it is invalid.
 import collections
 
 
-class Solution:
-    def isValidSudoku(self, board: list[list[str]]) -> bool:
-        bucket_x = collections.defaultdict(int)
-        bucket_y = collections.defaultdict(int)
-        # Row and Column Line Checks
-        for x in range(9):
-            for y in range(9):
-                # Row
-                coordinate_row = board[x][y]
-                if coordinate_row != ".":
-                    if coordinate_row not in bucket_y:
-                        bucket_y[coordinate_row] += 1
-                    else:
-                        print(f"row:{x + 1}, column:{y + 1}, value:{coordinate_row}. Repeated Value in Row Check!")
-                        return False
+def isValidSudoku(board: list[list[str]]) -> bool:
+    cols = collections.defaultdict(set)
+    rows = collections.defaultdict(set)
+    squares = collections.defaultdict(set)  # key = (r /3, c /3)
 
-                # Column
-                coordinate_column = board[y][x]
-                if coordinate_column != ".":
-                    if coordinate_column not in bucket_x:
-                        bucket_x[coordinate_column] += 1
-                    else:
-                        print(f"row:{y + 1}, column:{x + 1}, value:{coordinate_column} Repeated Value in Column Check!")
-                        return False
+    for r in range(9):
+        for c in range(9):
+            if board[r][c] == ".":
+                continue
+            if (
+                    board[r][c] in rows[r]
+                    or board[r][c] in cols[c]
+                    or board[r][c] in squares[(r // 3, c // 3)]
+            ):
+                return False
+            cols[c].add(board[r][c])
+            rows[r].add(board[r][c])
+            squares[(r // 3, c // 3)].add(board[r][c])
 
-            bucket_x.clear()
-            bucket_y.clear()
-
-            # Sub-box Checks
-            bucket_box = collections.defaultdict(int)
-            for x in range(3):
-                for y in range(3):
-                    bucket_box[board[(3 * x) + 0][(3 * y) + 0]] += 1
-                    bucket_box[board[(3 * x) + 0][(3 * y) + 1]] += 1
-                    bucket_box[board[(3 * x) + 0][(3 * y) + 2]] += 1
-
-                    bucket_box[board[(3 * x) + 1][(3 * y) + 0]] += 1
-                    bucket_box[board[(3 * x) + 1][(3 * y) + 1]] += 1
-                    bucket_box[board[(3 * x) + 1][(3 * y) + 2]] += 1
-
-                    bucket_box[board[(3 * x) + 2][(3 * y) + 0]] += 1
-                    bucket_box[board[(3 * x) + 2][(3 * y) + 1]] += 1
-                    bucket_box[board[(3 * x) + 2][(3 * y) + 2]] += 1
-
-                    bucket_box.pop(".")
-
-                    for value in bucket_box.values():
-                        if value > 1:
-                            return False
-
-                    bucket_box.clear()
-
-            return True
+    return True
 
 
-    """
-    
-    """
+if __name__ == '__main__':
+    board_2 = [["8", "3", ".", ".", "7", ".", ".", ".", "."],
+               ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+               [".", "9", "8", ".", ".", ".", ".", "6", "."],
+               ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+               ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+               ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+               [".", "6", ".", ".", ".", ".", "2", "8", "."],
+               [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+               [".", ".", ".", ".", "8", ".", ".", "7", "9"]]
+
+    print(isValidSudoku(board_2))
+
+"""
+Intuition
+The key insight for this problem is that each number (1-9) must appear exactly once in each row, column, and 3x3 sub-grid of the Sudoku board. This leads to the idea of checking these constraints for every cell in the board.
+
+Approach
+We initialize three sets for each of the columns, rows, and squares. We then iterate over each cell in the Sudoku board. For each non-empty cell, we check if its value is already in its respective row, column, or square. If it is, we know the Sudoku board is invalid, so we return False. If the cell's value isn't already in the corresponding sets, we add it and continue. If we can do this for all cells without returning False, we know the Sudoku board is valid, so we return True.
+
+Time complexity: O(1)
+While it may seem at first that the algorithm has O(n^2) complexity, given the nature of the Sudoku puzzle, the size of the board is fixed at 9x9. This means that the time complexity is constant - it does not grow with the size of the input and remains constant, so we say it is O(1).
+
+Space complexity: O(1)
+Similarly, while we do use some extra space to store the sets, the amount of space used does not grow with the size of the input - it's related to the size of the Sudoku board, which is always 9x9. Therefore, the space complexity is also O(1).
+"""

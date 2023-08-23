b0fee77a42b00c36696effb2b5a216bbbf379e0a
Gerardo Barcenas <gbarcenasjr@gmail.com>
2023-08-22 19:42:53 -0700

    Optimized the Solution for Question 128

--- Python-Solutions/q-0128.py
+++ Python-Solutions/q-0128.py
@@ -17,22 +17,47 @@ Output: 9
 
 
 def longestConsecutive(nums: list[int]) -> int:
-    if len(nums) <= 1:
-        return len(nums)
-    nums = sorted(set(nums))
-    longest = 1
-    counter = 1
-    for i in range(1, len(nums)):
-        if nums[i - 1] == nums[i] - 1:
-            counter += 1
-        else:
-            if counter > longest:
-                longest = counter
-            counter = 1
-    if counter > longest:
-        longest = counter
+    longest = 0
+    num_set = set(nums)
+
+    for number in num_set:
+        if number - 1 not in num_set:
+            current_number = number
+            current_streak = 1
+
+            while current_number + 1 in num_set:
+                current_number += 1
+                current_streak += 1
+
+            longest = max(longest, current_streak)
+
     return longest
 
 """
+Intuition
+The goal is to find the longest consecutive sequence of numbers in a given list. To solve this problem efficiently, 
+one can leverage the properties of sets to achieve O(1) time complexity for querying the presence of an element.
 
+Approach
+1. Convert the given list of numbers, `nums`, into a set (`num_set`) to achieve O(1) look-up times.
+2. For each `number` in `num_set`, check if it's the start of a sequence. This is done by ensuring the previous number 
+   (`number - 1`) is not in the set. This step ensures we don't count duplicates and ensures we only count each sequence 
+   once.
+3. If the `number` is the start of a sequence, loop to count how many numbers are in this sequence using a while loop.
+4. Keep track of the longest sequence found.
+
+Implementation Notes
+- By using sets, the look-up is efficient.
+- The check `number - 1 not in num_set` ensures that for a sequence like [1,2,3,4,5], only when we hit the number 1, 
+  will we start counting the sequence. For other numbers, this check will prevent unnecessary counting.
+- The inner while loop increments the `current_number` and counts the sequence until the next number isn't found in the 
+  set.
+
+Time Complexity: O(n)
+Though there's a nested loop structure, in the worst-case scenario, each number will be checked once as a start of a 
+sequence and then checked once again in the inner loop as part of some sequence. Thus, the operations inside the loops 
+are O(n).
+
+Space Complexity: O(n)
+An additional set, `num_set`, is created which can, in the worst case, have all n numbers from the original list.
 """
